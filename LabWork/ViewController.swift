725752a1a033eb00d70fcf0b7a523c7a774103ab
BironSu <bironsu@pursuit.org>
2018-11-14 14:24:21 -0500

    first commit

---
+++ LabWork/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LabWork
+//
+//  Created by Biron Su on 11/14/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    print("omg we are doing it")
+        
+    }
+
+
+}
+
